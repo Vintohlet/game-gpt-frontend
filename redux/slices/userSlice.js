e5caf67787fd2576b563a197ff57efb8c85089dc
Vintohlet <ruslantuleuoff@gmail.com>
2025-04-17 15:28:29 +0500

    fixed-baseUrl

--- redux/slices/userSlice.js
+++ redux/slices/userSlice.js
@@ -7,21 +7,21 @@ export const registerUser = createAsyncThunk(
   async (userData, { rejectWithValue }) => {
     try {
       const response = await axiosInstance.post("/auth/register", userData);
-      const { token, newUser } = response.data; // Исправлено на newUser
+      const { token, newUser } = response.data; 
 
       if (token) {
         localStorage.setItem("token", token);
-        localStorage.setItem("userId", newUser._id); // Исправлено на newUser._id
-        localStorage.setItem("userName", newUser.userName); // Исправлено на newUser.userName
-        localStorage.setItem("userEmail", newUser.email); // Исправлено на newUser.email
+        localStorage.setItem("userId", newUser._id); 
+        localStorage.setItem("userName", newUser.userName); 
+        localStorage.setItem("userEmail", newUser.email); 
         localStorage.setItem("isAuth", "true");
       }
 
       return {
         token,
-        userId: newUser._id, // Исправлено на newUser._id
-        userName: newUser.userName, // Исправлено на newUser.userName
-        userEmail: newUser.email, // Исправлено на newUser.email
+        userId: newUser._id, 
+        userName: newUser.userName, 
+        userEmail: newUser.email, 
       };
     } catch (error) {
       console.log("blyat" + error);
@@ -93,9 +93,9 @@ const userSlice = createSlice({
         state.loading = false;
         state.isAuth = true;
         state.token = action.payload.token;
-        state.userId = action.payload.userId; // Уже правильно
-        state.userName = action.payload.userName; // Уже правильно
-        state.userEmail = action.payload.userEmail; // Уже правильно
+        state.userId = action.payload.userId; 
+        state.userName = action.payload.userName; 
+        state.userEmail = action.payload.userEmail; 
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.loading = false;
--- services/axios.js
+++ services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"https://gamegpt-backend.onrender.com/",
+    baseURL:"https://gamegpt-backend.onrender.com",
     headers: {
         "Content-Type": "application/json",
       }
